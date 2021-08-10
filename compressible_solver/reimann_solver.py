364ec1a3141205f613272c236d3eeba02d5dfb73
Bevan W.S. Jones <thunder852@hotmail.com>
2021-08-10 21:16:03 +0200

    Added copyright notice and file headings.

--- compressible_solver/reimann_solver.py
+++ compressible_solver/reimann_solver.py
@@ -1,3 +1,20 @@
+# ----------------------------------------------------------------------------------------------------------------------
+#  This file is part of the SlowFlow distribution  (https://github.com/bevanwsjones/SlowFlow).
+#  (Copyright (c) 2020 Bevan Walter Stewart Jones.
+#
+#  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with this program. If not, see
+#  <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------------------------------------------------
+# filename: reimann_solver.py
+# description: todo
+# ----------------------------------------------------------------------------------------------------------------------
+
 import numpy as np
 from compressible_solver import node as cn
 
