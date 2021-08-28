Added centroid calculations for everything except hexagons. Bug in quadrilateral calculation. Changed mesh type to cell type in CellTable.

mesh/cell.py
@@ -56,17 +56,17 @@ class CellTable:
     The cell table, containing basic mesh cell geometry data.
     """
 
-    def __init__(self, _number_of_cells, _mesh_type):
+    def __init__(self, _number_of_cells, _cell_type):
         """
         Initialises, allocates the memory, for the cells in the mesh given a number of cells and the mesh type.
 
         :param _number_of_cells: Number of cells to allocate.
         :type _number_of_cells: int
-        :param _mesh_type: The type of mesh, triangle, quadrilaterals, etc.
-        :type _mesh_type: mt.MeshType
+        :param _cell_type: The type of mesh, triangle, quadrilaterals, etc.
+        :type _cell_type: mt.MeshType
         """
 
-        self.type = _mesh_type
+        self.type = _cell_type
         self.max_cell = _number_of_cells
         self.connected_face = -1 * np.ones(shape=[_number_of_cells, number_of_vertex_face(_mesh_type)], dtype=int)
         self.connected_vertex = -1 * np.ones(shape=[_number_of_cells, number_of_vertex_face(_mesh_type)], dtype=int)

mesh_preprocessor/finite_volume.py
@@ -12,47 +12,146 @@
 #  <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------------------------------------------------------
 # filename: finite_volume.py
-# description: todo
+# description: Contains functions for computing the the geometric properties of a finite volume mesh.
 # ----------------------------------------------------------------------------------------------------------------------
 
 import numpy as np
+from mesh import cell as cl
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Cell Central Co-Ordinate
 # ----------------------------------------------------------------------------------------------------------------------
 
-def calculate_edge_center(_cell_table, _vertex_table):
-    # triangle center
-    max_cell = _cell_table.max_cell
-    _cell_table.coordinate[0:max_cell] = (_vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 0]]
-                                          + _vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 1]]) / 2.0
+def calculate_edge_centroid(_cell_vertex_connectivity, _vertex_coordinates):
+    """
+    Computes the centroid of an edge cell, half way done.
+    
+    :param _cell_vertex_connectivity: Cell-vertex connectivity table, of the form [i_cell][list of vertices].
+    :param _cell_vertex_connectivity: np.array
+    :param _vertex_coordinates: Co-ordinates for all vertices in the mesh, of the form [i_vertex][x, y coordinates]
+    :param _vertex_coordinates: np.array
+    :return: List of the cell centroids of the form [i_cell][x, y coordinates].
+    :type: np.array
+    """""
+
+    max_cell = len(_cell_vertex_connectivity)
+    return (_vertex_coordinates[_cell_vertex_connectivity[0:max_cell, 0]]
+            + _vertex_coordinates[_cell_vertex_connectivity[0:max_cell, 1]]) / 2.0
 
 
-def calculate_triangle_center(_cell_table, _vertex_table):
-    # triangle center
-    max_cell = _cell_table.max_cell
-    _cell_table.coordinate[0:max_cell] = (_vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 0]]
-                                          + _vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 1]]
-                                          + _vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 2]]) / 3.0
+def calculate_triangle_centroid(_cell_vertex_connectivity, _vertex_coordinates):
+    """
+    Computes the centroid of an triangle, third of the sum of the co-ordinates.
+
+    :param _cell_vertex_connectivity: Cell-vertex connectivity table, of the form [i_cell][list of vertices].
+    :param _cell_vertex_connectivity: np.array
+    :param _vertex_coordinates: Co-ordinates for all vertices in the mesh, of the form [i_vertex][x, y coordinates]
+    :param _vertex_coordinates: np.array
+    :return: List of the cell centroids of the form [i_cell][x, y coordinates].
+    :type: np.array
+    """""
+
+    max_cell = len(_cell_vertex_connectivity)
+    return (_vertex_coordinates[_cell_vertex_connectivity[0:max_cell, 0]]
+            + _vertex_coordinates[_cell_vertex_connectivity[0:max_cell, 1]]
+            + _vertex_coordinates[_cell_vertex_connectivity[0:max_cell, 2]]) / 3.0
 
 
-def calculate_quadrilateral_center(_cell_table, _vertex_table):
-    # Quad center
-    max_cell = _cell_table.max_cell
-    _cell_table.coordinate[0:max_cell][0] = 0.50 * (
-            _vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 0]][0]
-            + _vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 1]][0])
-    _cell_table.coordinate[0:max_cell][1] = 0.50 * (
-            _vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 1]][1]
-            + _vertex_table.coordinate[_cell_table.connected_vertex[0:max_cell, 2]][1])
+def calculate_quadrilateral_centroid(_cell_vertex_connectivity, _vertex_coordinates):
+    """
+    Computes the centroid of a non-intersecting quadrilateral.
+
+    The method for computing the centroid is as follows a set of four triangles are formed. Each triangle is formed by
+    connecting three consecutive vertices in the quadrilateral. The centroids of these triangles form the co-ordinate
+     set, [[x_0, y_0],[x_1, y_1],[x_2, y_2],[x_3, y_3]]. The centroid is then the intersection of the lines given by
+     [X_4] = [X_2] + lambda_0 ([X_0] - [X_2]) | lambda_0 \in [0, 1],
+     [X_5] = [x_3] + lambda_1 ([X_1] - [X_3]) | lambda_1 \in [0, 1],
+     and where X denotes [x, y]. The intersection of these two lines is given by:
+
+          || x_0 y_0 |  x_0 - x_2 |
+          || x_2 y_2 |            |
+          || x_1 y_1 |  x_1 - x_3 |
+    x_c = || x_3 y_3 |            | ,
+          -------------------------
+          | x_0 - x_2   y_0 - y_2 |
+          | x_1 - x_3   y_1 - y_3 |
+
+          || x_0 y_0 |  y_0 - y_2 |
+          || x_2 y_2 |            |
+          || x_1 y_1 |  y_1 - y_3 |
+    y_c = || x_3 y_3 |            | .
+          -------------------------
+          | x_0 - x_2   y_0 - y_2 |
+          | x_1 - x_3   y_1 - y_3 |
+    For further reading see https://mathworld.wolfram.com/Line-LineIntersection.html
+
+    :param _cell_vertex_connectivity: Cell-vertex connectivity table, of the form [i_cell][list of vertices].
+    :param _cell_vertex_connectivity: np.array
+    :param _vertex_coordinates: Co-ordinates for all vertices in the mesh, of the form [i_vertex][x, y coordinates]
+    :param _vertex_coordinates: np.array
+    :return: List of the cell centroids of the form [i_cell][x, y coordinates].
+    :type: np.array
+    """
+
+    triangle_centre_0 = calculate_triangle_centroid(_cell_vertex_connectivity[:, [0, 1, 2]], _vertex_coordinates)
+    triangle_centre_1 = calculate_triangle_centroid(_cell_vertex_connectivity[:, [1, 2, 3]], _vertex_coordinates)
+    triangle_centre_2 = calculate_triangle_centroid(_cell_vertex_connectivity[:, [2, 3, 0]], _vertex_coordinates)
+    triangle_centre_3 = calculate_triangle_centroid(_cell_vertex_connectivity[:, [3, 0, 1]], _vertex_coordinates)
+
+    return np.stack((np.linalg.det(np.stack((
+        np.column_stack((np.linalg.det(np.stack((triangle_centre_0[:, ], triangle_centre_2[:, ]), axis=1)[:]),
+                         (triangle_centre_0[:, 0] - triangle_centre_2[:, 0])))[:],
+        np.column_stack((np.linalg.det(np.stack((triangle_centre_1[:, ], triangle_centre_3[:, ]), axis=1)[:]),
+                         (triangle_centre_1[:, 0] - triangle_centre_3[:, 0])))[:]), axis=1)),
+                     np.linalg.det(np.stack((np.column_stack((np.linalg.det(
+                         np.stack((triangle_centre_0[:, ], triangle_centre_2[:, ]), axis=1)[:]),
+                                                              (triangle_centre_0[:, 1] - triangle_centre_1[:, 1])))[:],
+                                             np.column_stack((np.linalg.det(
+                                                 np.stack((triangle_centre_1[:, ], triangle_centre_3[:, ]), axis=1)[:]),
+                                                              (triangle_centre_1[:, 1] - triangle_centre_3[:, 1])))[:]),
+                                            axis=1))), axis=1)[:] \
+           / np.linalg.det(
+        np.stack(((triangle_centre_0 - triangle_centre_2)[:], (triangle_centre_1 - triangle_centre_3)[:]), axis=1))[:]
 
 
-def calculate_hexagon_center(_cell_table, _vertex_table):
+def calculate_hexagon_centroid(_cell_vertex_connectivity, _vertex_coordinates):
+    """
+    Computes the centroid of a non-intersecting hexagon. TODO
+
+    :param _cell_vertex_connectivity: Cell-vertex connectivity table, of the form [i_cell][list of vertices].
+    :param _cell_vertex_connectivity: np.array
+    :param _vertex_coordinates: Co-ordinates for all vertices in the mesh, of the form [i_vertex][x, y coordinates]
+    :param _vertex_coordinates: np.array
+    :return: List of the cell centroids of the form [i_cell][x, y coordinates].
+    :type: np.array
+    """
+
     raise RuntimeError("Still a todo")
 
 
-def calculate_cell_center(_cell_table, _vertex_table):
-    raise RuntimeError("Still a todo")
+def calculate_cell_centroid(_cell_type, _cell_vertex_connectivity, _vertex_coordinates):
+    """
+    Computes the centroid of a cell based on the type of cell, edge, triangle, etc.
+
+    :param _cell_type: The type of cells described by the _cell_vertex_connectivity.
+    :type _cell_type: mesh.cell.CellType
+    :param _cell_vertex_connectivity: Cell-vertex connectivity table, of the form [i_cell][list of vertices].
+    :param _cell_vertex_connectivity: np.array
+    :param _vertex_coordinates: Co-ordinates for all vertices in the mesh, of the form [i_vertex][x, y coordinates]
+    :param _vertex_coordinates: np.array
+    :return: List of the cell centroids of the form [i_cell][x, y coordinates].
+    :type: np.array
+    """
+
+    if _cell_type == cl.CellType.edge:
+        return calculate_edge_centroid(_cell_vertex_connectivity, _vertex_coordinates)
+    elif _cell_type == cl.CellType.triangle:
+        return calculate_triangle_centroid(_cell_vertex_connectivity, _vertex_coordinates)
+    elif _cell_type == cl.CellType.quadrilateral:
+        return calculate_quadrilateral_centroid(_cell_vertex_connectivity, _vertex_coordinates)
+    elif _cell_type == cl.CellType.hexagon:
+        return calculate_hexagon_centroid(_cell_vertex_connectivity, _vertex_coordinates)
 
 
 # ----------------------------------------------------------------------------------------------------------------------