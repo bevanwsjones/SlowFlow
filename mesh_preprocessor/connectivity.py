number of faces and boundary faces function implemented and tested. Fixed bug in quadrilateral centroid calculation, and in test. Passing both tests.

mesh_preprocessor/connectivity.py
@@ -84,10 +84,29 @@ def connect_vertices_to_vertices(_cell_vertex_connectivity, _number_of_vertices)
 # Face connectivity
 # ----------------------------------------------------------------------------------------------------------------------
 
+def compute_number_of_faces(_vertex_cell_connectivity, _cell_vertex_connectivity):
+    """
+    Computes the number of faces and boundary faces in the mesh, creates a temporary face_vertex_connectivity table and
+    counts duplicates to determine the numbers.
 
-def compute_number_of_faces():
-    # todo  max_boundary_face must be calculated here.
-    return -1
+    :param _vertex_cell_connectivity: Vertex-cell connectivity table, of the form [i_vertex][list of cells].
+    :type _vertex_cell_connectivity: numpy.array
+    :param _cell_vertex_connectivity: Cell-vertex connectivity table, of the form [i_cell][list of vertices].
+    :type _cell_vertex_connectivity: numpy.array
+    :return: [number of faces, number of boundary faces]
+    :type: [int, int]
+    """
+
+    face_vertex_connectivity = np.empty(shape=[0, 2], dtype=int)
+    for cv_connectivity in _cell_vertex_connectivity:
+        for i_cv, i_vertex in enumerate(cv_connectivity):
+            face_vertex_connectivity = np.append(face_vertex_connectivity,
+                                                 [[i_vertex, cv_connectivity[i_cv - 1]]], axis=0)
+            face_vertex_connectivity[-1].sort()
+
+    number_of_boundary_faces = len(np.unique(face_vertex_connectivity))
+    number_of_faces = int((len(face_vertex_connectivity) - number_of_boundary_faces)/2 + number_of_boundary_faces)
+    return [number_of_faces, number_of_boundary_faces]
 
 
 def connect_faces_to_vertex(_cell_vertex_connectivity):
@@ -164,7 +183,6 @@ def connect_faces_to_cells(_vertex_cell_connectivity, _face_vertex_connectivity)
         if face_connectivity[1] != -1:
             face_connectivity.sort()
 
-    print(face_cell_connectivity)
     return face_cell_connectivity
 
 

mesh_preprocessor/finite_volume.py
@@ -102,21 +102,19 @@ def calculate_quadrilateral_centroid(_cell_vertex_connectivity, _vertex_coordina
     triangle_centre_2 = calculate_triangle_centroid(_cell_vertex_connectivity[:, [2, 3, 0]], _vertex_coordinates)
     triangle_centre_3 = calculate_triangle_centroid(_cell_vertex_connectivity[:, [3, 0, 1]], _vertex_coordinates)
 
-    return np.stack((np.linalg.det(np.stack((
-        np.column_stack((np.linalg.det(np.stack((triangle_centre_0[:, ], triangle_centre_2[:, ]), axis=1)[:]),
-                         (triangle_centre_0[:, 0] - triangle_centre_2[:, 0])))[:],
-        np.column_stack((np.linalg.det(np.stack((triangle_centre_1[:, ], triangle_centre_3[:, ]), axis=1)[:]),
-                         (triangle_centre_1[:, 0] - triangle_centre_3[:, 0])))[:]), axis=1)),
-                     np.linalg.det(np.stack((np.column_stack((np.linalg.det(
-                         np.stack((triangle_centre_0[:, ], triangle_centre_2[:, ]), axis=1)[:]),
-                                                              (triangle_centre_0[:, 1] - triangle_centre_1[:, 1])))[:],
-                                             np.column_stack((np.linalg.det(
-                                                 np.stack((triangle_centre_1[:, ], triangle_centre_3[:, ]), axis=1)[:]),
-                                                              (triangle_centre_1[:, 1] - triangle_centre_3[:, 1])))[:]),
-                                            axis=1))), axis=1)[:] \
-           / np.linalg.det(
-        np.stack(((triangle_centre_0 - triangle_centre_2)[:], (triangle_centre_1 - triangle_centre_3)[:]), axis=1))[:]
-
+    numerator_det_02 = np.linalg.det(np.stack((triangle_centre_0[:, ], triangle_centre_2[:, ]), axis=1))
+    numerator_det_13 = np.linalg.det(np.stack((triangle_centre_1[:, ], triangle_centre_3[:, ]), axis=1))
+    numerator_x_02 = triangle_centre_0[:, 0] - triangle_centre_2[:, 0]
+    numerator_x_13 = triangle_centre_1[:, 0] - triangle_centre_3[:, 0]
+    numerator_y_02 = triangle_centre_0[:, 1] - triangle_centre_2[:, 1]
+    numerator_y_13 = triangle_centre_1[:, 1] - triangle_centre_3[:, 1]
+    numerator_x = np.linalg.det(np.stack([np.column_stack([numerator_det_02[:, ], numerator_x_02[:, ]]),
+                                          np.column_stack([numerator_det_13[:, ], numerator_x_13[:, ]])], axis=1))
+    numerator_y = np.linalg.det(np.stack([np.column_stack([numerator_det_02[:, ], numerator_y_02[:, ]]),
+                                          np.column_stack([numerator_det_13[:, ], numerator_y_13[:, ]])], axis=1))
+    denominator = np.linalg.det(np.stack(((triangle_centre_0[:] - triangle_centre_2[:]),
+                                          (triangle_centre_1[:] - triangle_centre_3[:])), axis=1))
+    return np.stack([numerator_x[:]/denominator[:], numerator_y[:]/denominator[:]], axis=1)
 
 def calculate_hexagon_centroid(_cell_vertex_connectivity, _vertex_coordinates):
     """

mesh_preprocessor/tests/test_connectivity.py
@@ -86,7 +86,14 @@ class VertexConnectivityTest(ut.TestCase):
 class FaceConnectivityTest(ut.TestCase):
 
     def test_compute_number_of_faces(self):
-        return True
+        cell_vertex_connectivity = np.array([[0, 1, 2], [2, 1, 3], [3, 1, 4], [3, 4, 5]])
+        vertex_cell_connectivity = [np.array([0]), np.array([0, 1, 2]), np.array([0, 1]), np.array([1, 2, 3]),
+                                    np.array([2, 3]), np.array([3])]
+        [number_of_faces, number_of_boundary_faces] = ct.compute_number_of_faces(vertex_cell_connectivity,
+                                                                                 cell_vertex_connectivity)
+
+        self.assertEqual(9, number_of_faces)
+        self.assertEqual(6, number_of_boundary_faces)
 
     def test_connect_faces_to_vertex(self):
         cell_vertex_connectivity = np.array([[0, 1, 2], [2, 1, 3], [3, 1, 4], [3, 4, 5]])

mesh_preprocessor/tests/test_finite_volume.py
@@ -20,6 +20,7 @@ import unittest as ut
 import finite_volume as fv
 from mesh import cell as cl
 
+
 # ----------------------------------------------------------------------------------------------------------------------
 # Cell Geometry
 # ----------------------------------------------------------------------------------------------------------------------
@@ -65,8 +66,8 @@ class CellCentroidTest(ut.TestCase):
         self.assertAlmostEqual(base_y + (2.0 / 3.0) * height, cell_centre[1][1])
 
     def test_calculate_quadrilateral_center_regular(self):
-        vertex_coordinates = np.array([[0.5, 0.5], [1.0, 0.5], [1.0, 1.5], [0.5, 1.5], [2.0, 1.0], [2.0, 2.0]])
-        cell_vertex_connectivity = np.array([[0, 1, 2, 3], [3, 2, 4, 5]])
+        vertex_coordinates = np.array([[0.5, 0.5], [1.0, 0.5], [1.0, 1.5], [0.5, 1.5], [2.0, 1.5], [2.0, 2.5]])
+        cell_vertex_connectivity = np.array([[0, 1, 2, 3], [2, 1, 4, 5]])
         cell_centre = fv.calculate_quadrilateral_centroid(cell_vertex_connectivity, vertex_coordinates)
 
         # Check lengths
@@ -77,13 +78,12 @@ class CellCentroidTest(ut.TestCase):
         # check value
         base0 = [0.5, 0.5]
         base1 = [1.0, 0.5]
-        length0 = [0.5, 1.0]
-        length1 = [1.0, 1.0]
-        print(cell_centre)
-        self.assertAlmostEqual(base0[0] + (1.0 / 2.0) * length0[0], cell_centre[0][0])
-        self.assertAlmostEqual(base0[1] + (1.0 / 2.0) * length0[1], cell_centre[0][1])
-        self.assertAlmostEqual(base1[0] + (1.0 / 2.0) * length1[0], cell_centre[1][0])
-        self.assertAlmostEqual(base1[1] + (1.0 / 2.0) * length1[1], cell_centre[1][1])
+        bound_box_0 = [0.5, 1.0]
+        bound_box_1 = [1.0, 2.0]
+        self.assertAlmostEqual(base0[0] + (1.0 / 2.0)*bound_box_0[0], cell_centre[0][0])
+        self.assertAlmostEqual(base0[1] + (1.0 / 2.0)*bound_box_0[1], cell_centre[0][1])
+        self.assertAlmostEqual(base1[0] + (1.0 / 2.0)*bound_box_1[0], cell_centre[1][0])
+        self.assertAlmostEqual(base1[1] + (1.0 / 2.0)*bound_box_1[1], cell_centre[1][1])
 
     def test_calculate_quadrilateral_center_irregular(self):
         vertex_coordinates = np.array([[0.0, 0.0], [1.0, 0.0], [1.5, 1.5], [0.5, 1.0]])