0c0e970a5489aa214f5d3e862ec79a4bf6878321
Bevan W.S. Jones <thunder852@hotmail.com>
2021-09-30 07:29:22 +0200

    Created error_analysis.py and associated test file. Added l_norm_finite_volume to the error_analysis.py.

---
+++ post_processor/error_analysis.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------
+#  This file is part of the SlowFlow distribution  (https://github.com/bevanwsjones/SlowFlow).
+#  Copyright (c) 2020 Bevan Walter Stewart Jones.
+#
+#  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with this program. If not, see
+#  <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------------------------------------------------
+# filename: error_analysis.py
+# description: todo
+# ----------------------------------------------------------------------------------------------------------------------
+
+import numpy as np
+
+
+def l_norm_finite_volume(_field_error, _cell_volume, _order):
+    return np.linalg.norm(_field_error[:] * _cell_volume[:], ord=_order) / np.sum(_cell_volume)
