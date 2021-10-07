7f32ca8f9cb05dd89a331d8c925952afe230580a
kko27 <kwangbemk@gmail.com>
2021-10-07 09:11:21 +0200

    minor edits

--- main3.py
+++ main3.py
@@ -4,7 +4,7 @@ from post_processor import graph as gr
 
 # cartesian grid error calculator and plotter
 cells_matrix = np.array([[3, 3], [9, 9], [30, 30], [30, 30]])
-#g = rp.cartesian_error(cells_matrix, met = 2)
+#g = rp.cartesian_error(cells_matrix, met = 3)
 
 # Grid Quality Experiments
 def uneven_experiment():
@@ -12,7 +12,7 @@ def uneven_experiment():
     cells_matrix = np.array([60, 60])
     uneven_matrix = np.array([[0.8, 0.8], [0.85, 0.85], [0.9, 0.9], [0.95, 0.95], [0.98, 0.98]])
     #uneven_matrix = np.array([[0.6, 0.6], [0.9, 0.9], [1.5, 1.5], [2.0, 2.0], [3.0, 3.0]])
-    rp.single_grid_metric(cells_matrix, uneven_matrix, grid_quality = 1, met=0)
+    rp.single_grid_metric(cells_matrix, uneven_matrix, grid_quality = 1, met=1)
     # rp.quality_error(cells_matrix, uneven_matrix, grid_quality = 1, met = 0)
     return -1
 def non_orthogonal_experiment():
@@ -20,7 +20,7 @@ def non_orthogonal_experiment():
     cells_matrix = np.array([90, 90])
     skew_matrix = np.array([[0.1, 0.1], [0.3, 0.3], [0.5, 0.5], [0.6, 0.6], [0.7, 0.7], [0.8, 0.8],[0.9, 0.9]])
     #skew_matrix = np.array([[0.0, 0.1], [0.0, 0.3], [0.0, 0.5], [0.0, 0.6], [0.0, 0.7], [0.0, 0.8], [0.0, 0.9]])
-    rp.single_grid_metric(cells_matrix, skew_matrix, grid_quality = 0, met=1)
+    rp.single_grid_metric(cells_matrix, skew_matrix, grid_quality = 0, met=3)
     # rp.quality_error(cells_matrix, skew_matrix, 0, met = 0)
     return - 1
 def skewness_experiment():
@@ -39,21 +39,23 @@ non_orthogonal_experiment()
 
 def nonorth_refine_experiment():
     cells_matrix = np.array([[3, 3], [9, 9], [30, 30], [50, 50]])
-    skew_matrix = np.array([[0.0, 0.0], [0.1, 0.2], [0.3, 0.5], [0.8, 0.5], [0.9, 0.7], [0.9, 0.9]])
+    #skew_matrix = np.array([[0.0, 0.0], [0.1, 0.2], [0.3, 0.5], [0.8, 0.5], [0.9, 0.7], [0.9, 0.9]])
+    skew_matrix = np.array([[0.0, 0.1], [0.0, 0.3], [0.0, 0.5], [0.0, 0.6], [0.0, 0.7], [0.0, 0.8], [0.0, 0.9]])
     rp.grid_refinement_error(cells_matrix, skew_matrix, grid_quality=0, met = 2)
 
 def uneven_refine_experiment():
-    cells_matrix = np.array([[3, 3], [9, 9], [30, 30], [60, 60]])
-    uneven_matrix = np.array([[0.8, 0.8], [0.9, 0.9], [1.2, 1.2], [1.5, 1.5]])
+    cells_matrix = np.array([[3, 3], [9, 9], [30, 30], [60, 60], [70, 70]])
+    #uneven_matrix = np.array([[0.8, 0.8], [0.9, 0.9], [1.2, 1.2], [1.5, 1.5]])
+    uneven_matrix = np.array([[0.8, 0.8], [0.85, 0.85], [0.9, 0.9], [0.95, 0.95], [0.98, 0.98]])
     #uneven_matrix = np.array([[0.4, 0.4], [0.7, 0.7], [0.9, 0.9], [1.2, 1.2], [1.5, 1.5]])
-    rp.grid_refinement_error(cells_matrix, uneven_matrix, grid_quality=1, met = 2)
+    rp.grid_refinement_error(cells_matrix, uneven_matrix, grid_quality=1, met = 3)
 
 def skewness_refine_experiment():
     cells_matrix = np.array([[3, 3], [9, 9], [30, 30], [45, 45]])
     skew_matrix = np.array([0.1, 0.2, 0.5, 1.0, 1.2])
-    rp.grid_refinement_error(cells_matrix, skew_matrix, grid_quality=2, met = 2)
+    rp.grid_refinement_error(cells_matrix, skew_matrix, grid_quality=2, met = 3)
 
-# nonorth_refine_experiment()
+#nonorth_refine_experiment()
 # uneven_refine_experiment()
 # skewness_refine_experiment()
 
